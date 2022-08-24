df5ff64aa77054248c8ff0e1917c77609add4275
Jamie Mortensen <16mortensenj@gmail.com>
2022-08-24 19:38:45 -0400

    layed out second activity

--- app/src/main/java/com/example/lab7/NewPostActivity.java
+++ app/src/main/java/com/example/lab7/NewPostActivity.java
@@ -1,9 +1,14 @@
 package com.example.lab7;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class NewPostActivity extends AppCompatActivity {
 
@@ -18,5 +23,16 @@ public class NewPostActivity extends AppCompatActivity {
         ab.setDisplayHomeAsUpEnabled(true);
     }
 
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void addNewPost(View v){
+        ServerInterface.Posts.sendPost(1,
+                findViewById(R.id.post_title).toString(),
+                findViewById(R.id.post_content).toString(),
+                getApplicationContext());
+        MainActivity activity =(MainActivity)getParent();
+        activity.refreshRecyclerView();
+        finish();
+    }
 
 }
